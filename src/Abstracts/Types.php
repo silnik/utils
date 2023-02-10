e73ab00f0fe893ad447acd9148f1ff1b63195db3
bieleski <clebersonbieleski@gmail.com>
2023-02-10 20:20:07 -0300

    insert types

---
+++ src/Abstracts/Types.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Silnik\Utils\Abstracts;
+
+abstract class Types
+{
+    final public const INT = 'int';
+    final public const STRING = 'string';
+    final public const BOOL = 'bool';
+    final public const FLOAT = 'float';
+    final public const ARRAY = 'array';
+}
